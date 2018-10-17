5610b89b96472f9cdee4d66a5b2939a54ccdf5dd
ygjack <ygjack414@hotmail.com>
2018-10-17 13:03:42 +0800

    fix autowidth with "auto" issue

--- src/plugins/autoWidth.js
+++ src/plugins/autoWidth.js
@@ -1,6 +1,6 @@
 export default {
   workcellCreated ({ worksheet, colRange, cell, cellStyle }) {
-    if (colRange.from === colRange.to) {
+    if (colRange.from === colRange.to && cellStyle.width !== 'auto') {
       worksheet.getColumn(colRange.from + 1).width = (+cellStyle.width.split('px')[0]) * this.options.widthRatio
     }
   }
